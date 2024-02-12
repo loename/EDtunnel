620279210ec985656867636a4ebdadb96d752e1c
loename <159006705+loename@users.noreply.github.com>
2024-02-13 01:29:07 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '8ea08570-2bc9-45e0-875b-54200dc91ef3';
 
-const proxyIPs = ['198.13.59.119'];
+const proxyIPs = ['178.128.40.54'];
 
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
